7f8cfe5aecd09645991023f5aa375aa3a4dbd6b2
Huraira-Code <osama.shahid.k.pk@gmail.com>
2025-03-21 06:24:33 +0500

    Update app.js

--- Backened/src/app.js
+++ Backened/src/app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(cors({
     origin: '*',
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:false,            //access-control-allow-credentials:true
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
